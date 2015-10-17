13843e0fc60fddf3efddd69c54b805de65ba658c
Michał Budzyński <michal@virtualdesign.pl>
2015-10-17 01:01:00 +0100

    :construction_worker: login card

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+var dev = true;
+
 var removePreloader = function(){
+  if (dev) {
+    $('.proto-preloader').remove();
+    return;
+  }
   setTimeout(function(){
     $('.proto-preloader').css('opacity', 0);
     $('.proto-preloader').one('transitionend', function(){
       $('.proto-preloader').remove();
       console.log('removed');
     });
-  }, 1000);
+  }, 0);
 };
 
 
